c665ad23296bb13f04aec6789759f12685996cae
lora4423 <loratabak9@gmail.com>
2023-10-18 17:20:41 +0200

    Zadatak 1 part 1

--- Projekt/main/main.c
+++ Projekt/main/main.c
@@ -1,7 +1,36 @@
-#include <stdio.h>
+﻿#include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
-	
+#define MAX_NAME_SIZE 64
+#define MAX_LINE 1024
+
+typedef struct _Student {
+
+	char m_Ime[MAX_NAME_SIZE];
+	char m_Prezime[MAX_NAME_SIZE];
+	int m_Bodovi;
+} Student;
+
+int main() {
+
+	FILE* fileHandle = NULL;
+	fopen_s(&fileHandle, "studenti.txt", "r");
+	if (fileHandle == NULL) {
+		printf("Nemoguće otvoriti datoteku.\n");
+		return -1;
+	}
+
+	char curLineBuf[MAX_LINE] = { 0 };
+	int numStudents = 0;
+	while (!feof(fileHandle))
+	{
+		numStudents++;
+		fgets(curLineBuf, MAX_LINE, fileHandle);
+		// printf("Line: %s", curLineBuf);
+	}
+
+	printf("Number of lines: %d", numStudents);
+
+	fclose(fileHandle);
 	return 0;
 }
